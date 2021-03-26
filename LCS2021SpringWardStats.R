57018f85fef9a3a02987670967bc01a0f5c5ab34
baileykui <81328152+baileykui@users.noreply.github.com>
2021-03-25 18:13:25 -0700

    Update LCS2021SpringWardStats.R

--- LCS2021SpringWardStats.R
+++ LCS2021SpringWardStats.R
@@ -1,51 +1,61 @@
+# libraries used
 library(tidyverse)
 library(rvest)
 library(dplyr)
 
-# creates a session and gets links and names of each region, season, and year for league of legends pro matches
-s <- html_session("https://lol.gamepedia.com/League_Championship_Series")
-region_year_season_link <- s %>% html_nodes(".wikitable:nth-child(7) td > .to_hasTooltip") %>% html_attr("href")
-region_year_season_text <- s %>% html_nodes(".wikitable:nth-child(7) td > .to_hasTooltip") %>% html_text()
+link <- "https://lol.gamepedia.com/LCS/2021_Season/Spring_Season/Match_History"
 
-# gets overview match history link for all LCS 2021 Spring season
-match_history_link <- s %>%
-  jump_to(paste0("/LCS/2021_Season/Spring_Season", "/Match_History")) %>% 
-  html_nodes("td:nth-child(13) .text") %>% 
-  html_attr("href") 
-
-# changes match history overview to match history stats link
-if (!all(str_detect(match_history_link, "&tab=overview"))) {
-  tfvec <- str_detect(match_history_link, "&tab=overview")
-  match_history_link[which(!tfvec)] <- paste0(match_history_link[which(!tfvec)], "&tab=stats")
-  match_history_link <- str_replace(match_history_link, "&tab=overview", "&tab=stats")
-  match_history_link <- as.data.frame(match_history_link)
+match_history_stats <- function(gamepedia_link) {
+  s <- html_session(gamepedia_link)
+  
+  # gets overview match history link for all games
+  match_history_link <- s %>%
+    html_nodes("td:nth-child(13) .text") %>% 
+    html_attr("href") 
+  
+  # changes match history overview to match history stats link
+  if (!all(str_detect(match_history_link, "&tab=overview"))) {
+    tfvec <- str_detect(match_history_link, "&tab=overview")
+    match_history_link[which(!tfvec)] <- paste0(match_history_link[which(!tfvec)], "&tab=stats")
+    match_history_link <- str_replace(match_history_link, "&tab=overview",
+                                      "&tab=stats")
+    match_history_link <- as.data.frame(match_history_link)
+  }
+  match_history_link
 }
 
-# gets unique player names
-player_names <- s %>% 
-  jump_to(paste0(region_year_season_link[4], "/Match_History")) %>% 
-  html_nodes(".pWAN") %>% 
-  html_text()
-player_mat <- matrix(0, byrow = TRUE, ncol = 10)
-player_df <- as.data.frame(player_mat)
-player_df[1, ] <- player_names[(1:10)]
-for (i in seq_along(player_names)) {
-  player_df[1+i, ] <- player_names[(i*10 + 1):(i*10 + 10)]
+get_players <- function(gamepedia_link) {
+  s <- html_session(gamepedia_link)
+  player_names <- s %>% 
+    html_nodes(".pWAN") %>% 
+    html_text()
+  
+  player_mat <- matrix(0, byrow = TRUE, ncol = 10)
+  player_df <- as.data.frame(player_mat)
+  player_df[1, ] <- player_names[(1:10)]
+  for (i in seq_along(player_names)) {
+    player_df[1+i, ] <- player_names[(i*10 + 1):(i*10 + 10)]
+  }
+  player_df <- player_df[complete.cases(player_df),]
+  colnames(player_df) <- c("BTop", "BJungle", "BMid", "BBot", "BSupport",
+                           "RTop", "RJungle", "RMid", "RBot", "RSupport")
+  
+  player_df
 }
-player_df <- player_df[complete.cases(player_df),]
-colnames(player_df) <- c("BTop", "BJungle", "BMid", "BBot", "BSupport", "RTop", "RJungle", "RMid", "RBot", "RSupport")
 
 # binds match history stats link with players' warding stats
-player_match <- cbind(match_history_link, player_df)
+player_match <- cbind(match_history_stats(link),
+                      get_players(link))
 
 # reads in raw warding statistics 
 # manually copy and pasted from each match history stats link 
 # into a google sheets and converted into a csv file
 raw_ward <- read.csv("LCS2021SpringWardStatsRaw.csv", header = FALSE)
-colnames(raw_ward) <- c("Ward", c("BTop", "BJungle", "BMid", "BBot", "BSupport", "RTop", "RJungle", "RMid", "RBot", "RSupport"))
+colnames(raw_ward) <- c("Ward", c("BTop", "BJungle", "BMid", "BBot", "BSupport",
+                                  "RTop", "RJungle", "RMid", "RBot", "RSupport"))
 
 # adds an empty column in the 1st column
-player_df <- cbind("", player_df)
+player_df <- cbind("", get_players(link))
 
 # assigns player names to their warding statistics
 game1 <- raw_ward[1:3,]
@@ -111,6 +121,7 @@ total_wards_stats <- rbind(control_wards_purchased,
                            wards_destroyed, wards_placed)
 rownames(total_wards_stats) <- c("ControlWardsPurchased",
                                  "WardsDestroyed", "WardsPlaced")
+total_wards_stats
 
 # barplot seeing total wards place by side and role of a player
 par(mar = c(5, 5, 2, 3))
@@ -122,11 +133,6 @@ barplot(as.matrix(total_wards_stats),
 legend("bottomright",
        c("ControlWardsPurchased", "WardsDestroyed", "WardsPlaced"),
        fill = c("red2", "grey", "lightgreen"))
-
-# warding stats by side
-blue_raw_ward <- raw_ward[,1:6]
-red_raw_ward <- cbind(raw_ward[,1], raw_ward[,7:11])
-colnames(red_raw_ward) <- c("Ward", "RTop", "RJungle", "RMid", "RBot", "RSupport")
 
 # converts master list into a data frame 
 master_df <- bind_rows(master_list)
@@ -171,6 +177,7 @@ total_wp <- t_total_wp
 
 # clean warding stat totals by player
 total_wards_stats_player <- cbind(total_cwp, total_wd, total_wp)
+total_wards_stats_player
 
 # max control wards purchased by player
 total_wards_stats_player[which.max(total_wards_stats_player[,1]),]
@@ -194,26 +201,20 @@ fly_player_ward_stats <- total_wards_stats_player[c(41:45, 51),]
 dig_player_ward_stats <- total_wards_stats_player[46:50,]
 
 # sums player warding stats
-imt_ward_stats <- apply(imt_player_ward_stats, 2, sum)
-c9_ward_stats <- apply(c9_player_ward_stats, 2, sum)
-eg_ward_stats <- apply(eg_player_ward_stats, 2, sum)
-clg_ward_stats <- apply(clg_player_ward_stats, 2, sum)
-onet_ward_stats <- apply(onet_player_ward_stats, 2, sum)
-tl_ward_stats <- apply(tl_player_ward_stats, 2, sum)
-gg_ward_stats <- apply(gg_player_ward_stats, 2, sum)
-tsm_ward_stats <- apply(tsm_player_ward_stats, 2, sum)
-fly_ward_stats <- apply(fly_player_ward_stats, 2, sum)
-dig_ward_stats <- apply(dig_player_ward_stats, 2, sum)
+imt <- apply(imt_player_ward_stats, 2, sum)
+c9 <- apply(c9_player_ward_stats, 2, sum)
+eg <- apply(eg_player_ward_stats, 2, sum)
+clg <- apply(clg_player_ward_stats, 2, sum)
+onet <- apply(onet_player_ward_stats, 2, sum)
+tl <- apply(tl_player_ward_stats, 2, sum)
+gg <- apply(gg_player_ward_stats, 2, sum)
+tsm <- apply(tsm_player_ward_stats, 2, sum)
+fly <- apply(fly_player_ward_stats, 2, sum)
+dig <- apply(dig_player_ward_stats, 2, sum)
 
 # data frame of all LCS teams warding stats
-lcs_ward_stats <- as.data.frame(rbind(imt_ward_stats, c9_ward_stats, eg_ward_stats, clg_ward_stats, onet_ward_stats, tl_ward_stats, gg_ward_stats, tsm_ward_stats, fly_ward_stats, dig_ward_stats))
-
-# gold spent on control wards 
-# note players can undo the control ward purchase and the counter will stay the same
-lcs_ward_stats_g <- lcs_ward_stats %>% 
-  mutate(
-    GoldSpentCW = ControlWardsPurchased * 75
-  )
+lcs_ward_stats <- as.data.frame(rbind(imt, c9, eg, clg, onet, 
+                                      tl,gg, tsm, fly, dig))
 
 # max control wards purchased by team
 lcs_ward_stats[which.max(lcs_ward_stats[,1]),]
@@ -224,5 +225,7 @@ lcs_ward_stats[which.max(lcs_ward_stats[,2]),]
 # max wards placed by team
 lcs_ward_stats[which.max(lcs_ward_stats[,3]),]
 
+# makes csv files
 write.csv(total_wards_stats_player, "LCS2021SpringPlayerWardStats.csv")
 write.csv(lcs_ward_stats, "LCS2021SpringTeamWardStats.csv")
+
